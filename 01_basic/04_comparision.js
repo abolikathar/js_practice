8a1274dacddd7f6da1e75b1d83ebd954ef27d4a9
Aboli kathar <100370887+abolikathar@users.noreply.github.com>
2025-03-13 06:11:12 +0000

    comparision

---
+++ 01_basic/04_comparision.js
@@ -0,0 +1,17 @@
+/*
+console.log(2>1); //true
+console.log(2>=1);//true
+console.log(2<=1);//false
+console.log(2==1);//false
+console.log(2!=1);//true
+*/
+
+//avoid below type of comparision
+/*
+console.log("2">1); //true
+console.log("02">1);
+
+console.log(null>0); //false
+console.log(null==0);//false
+console.log(null >= 0);//true
+*/
